0dfb7e45056184eff0b45470a184f1adf6c2ac3d
SmaugiD <TheSmaug@protonmail.com>
2019-08-29 17:52:58 +0200

    Fix pathing

---
+++ metadata---documentation-docs-doc-1055-306.ddd3a87f84e0650d9219.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{49:function(o){o.exports=JSON.parse('{"id":"doc1","title":"Style Guide","sidebar_label":"Style Guide","description":"You can write content using [GitHub-flavored Markdown syntax](https://github.github.com/gfm/).\\r","source":"@site/docs\\\\doc1.md","permalink":"/documentation/docs/doc1"}')}}]);
