86ecb62cf833462bd70fc8f77c11733a7c2cacd3
SmaugiD <TheSmaug@protonmail.com>
2019-08-29 18:40:00 +0200

    Hopefuly final folder fix

---
+++ metadata---docs-mdx-253-242.3935a4d175eee0abdf5e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[33],{55:function(o){o.exports=JSON.parse('{"id":"mdx","title":"Powered by MDX","description":"You can write JSX and use React components within your Markdown thanks to [MDX](https://mdxjs.com/).\\r","source":"@site/docs\\\\mdx.md","permalink":"/docs/mdx"}')}}]);
